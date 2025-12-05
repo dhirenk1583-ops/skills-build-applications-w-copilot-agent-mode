9874c3b837bea47a8fa6f7a99430882ee254bdde
dhirenk1583-ops <dhirenk1583@gmail.com>
2025-12-05 03:54:00 +0000

    Updated

---
+++ octofit-tracker/frontend/app/src/components/OctofitLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const OctofitLogo = () => (
+  <img src={logo} alt="OctoFit Logo" className="octofit-logo" />
+);
+
+export default OctofitLogo;
